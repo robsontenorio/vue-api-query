1d19508e3da3e677919608b06a2f8a45080ed405
Robson Tenório <robsontenorio@me.com>
2018-03-29 02:55:10 -0300

    tests

--- tests/model.test.js
+++ tests/model.test.js
@@ -1,3 +1,4 @@
+import qs from 'qs'
 import Post from './dummy/models/Post'
 
 describe('Model methods', () => {
@@ -10,5 +11,19 @@ describe('Model methods', () => {
     }
 
     expect(errorModel).toThrow('The "id" must be a integer on find() method.')
+  })
+
+  test('it builds a complexy query', () => {
+    const post = Post
+      .where('title', 'Cool')
+      .where('status', 'ACTIVE')
+      .include('user')
+      .append('likes')
+      .orderBy('created_at')
+
+    const query = encodeURI('?include=user&filter[title]=Cool&filter[status]=ACTIVE&append=likes&sort=created_at')
+
+    expect(post._builder.query()).toEqual(query)
+
   })
 })
