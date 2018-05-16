aefdc80268aebff11f94b72a9ff6621d9bcd1af4
Robson Tenório <robsontenorio@MacBook-Pro-de-Robson.local>
2018-05-16 15:13:08 -0300

    Add `primaryKey()` method. In case your model does not work with default primary key ('id')

--- tests/dummy/data/posts.js
+++ tests/dummy/data/posts.js
@@ -1,12 +1,14 @@
 export const Posts = [
   {
     id: 1,
+    someId: 'du761-8bc98',
     firstname: 'John',
     lastname: 'Doe',
     age: 25
   },
   {
     id: 1,
+    someId: 'pa813-7jx02',
     firstname: 'Mary',
     lastname: 'Doe',
     age: 25
