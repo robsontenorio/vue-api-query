a520df403ff5996cfb41a70349fd75414e0bd5dc
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-10-20 14:35:48 -0300

    test: fix builder test

--- tests/builder.test.ts
+++ tests/builder.test.ts
@@ -1,13 +1,15 @@
-import Post from './dummy/models/Post'
-import ModelWithParamNames from './dummy/models/ModelWithParamNames'
-import { Model } from '../src'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 
+import Config from '../src/Config'
+import ModelWithParamNames from './dummy/models/ModelWithParamNames'
+import Post from './dummy/models/Post'
+import User from './dummy/models/User'
+
 describe('Query builder', () => {
-  let errorModel = {}
-  Model.$http = axios
-  let axiosMock = new MockAdapter(axios)
+  const errorModel = {}
+  Config.$http = axios
+  const axiosMock = new MockAdapter(axios)
 
   beforeEach(() => {
     axiosMock.reset()
@@ -126,7 +128,7 @@ describe('Query builder', () => {
   })
 
   test('where() throws a exception when second parameter is not primitive', () => {
-    let errorModel = () => {
+    const errorModel = () => {
       const post = Post.where('id', ['foo'])
     }
 
@@ -134,14 +136,14 @@ describe('Query builder', () => {
   })
 
   test('whereIn() sets properly the builder', () => {
-    let post = Post.whereIn('status', ['ACTIVE', 'ARCHIVED'])
+    const post = Post.whereIn('status', ['ACTIVE', 'ARCHIVED'])
 
     // @ts-ignore
     expect(post._builder.filters).toEqual({ status: 'ACTIVE,ARCHIVED' })
   })
 
   test('whereIn() throws a exception when second parameter is not a array', () => {
-    let errorModel = () => {
+    const errorModel = () => {
       // @ts-ignore
       const post = Post.whereIn('id', 'foo')
     }
@@ -152,14 +154,14 @@ describe('Query builder', () => {
   })
 
   test('page() sets properly the builder', () => {
-    let post = Post.page(3)
+    const post = Post.page(3)
 
     // @ts-ignore
     expect(post._builder.pageValue).toEqual(3)
   })
 
   test('page() throws a exception when value is not a number', () => {
-    let errorModel = () => {
+    const errorModel = () => {
       // @ts-ignore
       const post = Post.page('foo')
     }
@@ -168,14 +170,14 @@ describe('Query builder', () => {
   })
 
   test('limit() sets properly the builder', () => {
-    let post = Post.limit(10)
+    const post = Post.limit(10)
 
     // @ts-ignore
     expect(post._builder.limitValue).toEqual(10)
   })
 
   test('limit() throws a exception when value is not a number', () => {
-    let errorModel = () => {
+    const errorModel = () => {
       // @ts-ignore
       const post = Post.limit('foo')
     }
@@ -186,7 +188,7 @@ describe('Query builder', () => {
   })
 
   test('select() with no parameters', () => {
-    let errorModel = () => {
+    const errorModel = () => {
       const post = Post.select()
     }
 
@@ -196,14 +198,14 @@ describe('Query builder', () => {
   })
 
   test('select() for single entity', () => {
-    let post = Post.select('age', 'firstname')
+    const user = User.select('age', 'firstname')
 
     // @ts-ignore
-    expect(post._builder.fields.posts).toEqual('age,firstname')
+    expect(user._builder.fields.posts).toEqual('age,firstname')
   })
 
   test('select() for related entities', () => {
-    let post = Post.select({
+    const post = Post.select({
       posts: ['title', 'content'],
       user: ['age', 'firstname']
     })
@@ -215,14 +217,14 @@ describe('Query builder', () => {
   })
 
   test('params() sets properly the builder', () => {
-    let post = Post.params({ doSomething: 'yes' })
+    const post = Post.params({ doSomething: 'yes' })
 
     // @ts-ignore
     expect(post._builder.payload).toEqual({ doSomething: 'yes' })
   })
 
   test('params() throws a exception when the payload is not an object', () => {
-    let errorModel = () => {
+    const errorModel = () => {
       // @ts-ignore
       const post = Post.params()
     }
