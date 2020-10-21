0fcd05f02b1e22b2b65eda415baf02957fb9fcaf
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-10-21 13:29:46 -0300

    test(model): refactor Tag model

--- tests/dummy/models/Tag.js
+++
@@ -1,5 +0,0 @@
-import BaseModel from './BaseModel'
-
-export default class Tag extends BaseModel {
-  //
-}
---
+++ tests/dummy/models/Tag.ts
@@ -0,0 +1,5 @@
+import BaseModel from './BaseModel'
+
+export default class Tag extends BaseModel<false, false>() {
+  //
+}
