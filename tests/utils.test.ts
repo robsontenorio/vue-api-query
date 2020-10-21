49a8929bfbdd8b12f403075110bdcde1c3e12849
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-10-21 15:34:21 -0300

    test(utils): add more tests

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -15,6 +15,42 @@ describe('Utilities', () => {
     expect(result).toBe(1)
   })
 
+  test('Get property defined by simple string.', () => {
+    const holder = {
+      a: {
+        b: {
+          c: 1
+        }
+      }
+    }
+
+    const result = getProp(holder, 'a')
+
+    expect(result).toBe(holder.a)
+  })
+
+  test('Get holder if propName is not defined.', () => {
+    const holder = {
+      a: {
+        b: {
+          c: 1
+        }
+      }
+    }
+
+    // @ts-ignore
+    const result = getProp(holder)
+
+    expect(result).toBe(holder)
+  })
+
+  test('Get empty object if holder is not defined.', () => {
+    // @ts-ignore
+    const result = getProp()
+
+    expect(result).toStrictEqual({})
+  })
+
   test('Set property defined by dot notation in string.', () => {
     const holder = {
       a: {
@@ -27,6 +63,22 @@ describe('Utilities', () => {
     setProp(holder, 'a.b.c', 2)
 
     const result = getProp(holder, 'a.b.c')
+
+    expect(result).toBe(2)
+  })
+
+  test('Set property defined by simple string.', () => {
+    const holder = {
+      a: {
+        b: {
+          c: 1
+        }
+      }
+    }
+
+    setProp(holder, 'a', 2)
+
+    const result = getProp(holder, 'a')
 
     expect(result).toBe(2)
   })
