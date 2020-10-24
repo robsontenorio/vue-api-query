6b6e2cda69874afc2aab02ecc506e3be3b7551d1
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-10-24 15:41:34 -0300

    feat: export type HTTPRequestConfig

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import Model from './Model'
 export { Model }
-export { QueryResponseCollection, QueryResponseModel } from './types'
+export {
+  QueryResponseCollection,
+  QueryResponseModel,
+  HTTPRequestConfig
+} from './types'
