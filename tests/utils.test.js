4daf501dad8203e26fbe3f3ddfdf39c856422c46
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-11-05 15:05:13 -0300

    feat(utils): add `serialize` utility
    Convenient JavaScript function that serializes Objects to FormData instances.
    
    Based on https://github.com/therealparmesh/object-to-formdata (MIT)

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,6 +1,12 @@
-import { getProp, setProp } from '../src/utils'
+import { getProp, serialize, setProp } from '../src/utils'
 
 describe('Utilities', () => {
+  const formDataAppend = global.FormData.prototype.append;
+
+  beforeEach(() => {
+    global.FormData.prototype.append = jest.fn(formDataAppend);
+  });
+
   /**
    * Tests of `getProp`
    * Based on tests from https://github.com/dy/dotprop (MIT)
@@ -183,4 +189,282 @@ describe('Utilities', () => {
 
     expect(foo.a.b.d).toStrictEqual({ e: 5, z: [1, 2, 3, 4] })
   })
+
+  /**
+   * Tests of `serialize`
+   * Based on tests from https://github.com/therealparmesh/object-to-formdata (MIT)
+   */
+
+  test('[serialize]: undefined', () => {
+    const formData = serialize({
+      foo: undefined,
+    });
+
+    expect(formData.append).not.toHaveBeenCalled();
+    expect(formData.get('foo')).toBe(null);
+  });
+
+  test('[serialize]: null', () => {
+    const formData = serialize({
+      foo: null,
+    });
+
+    expect(formData.append).toHaveBeenCalledTimes(1);
+    expect(formData.append).toHaveBeenCalledWith('foo', '');
+    expect(formData.get('foo')).toBe('');
+  });
+
+  test('[serialize]: null with nullsAsUndefineds option', () => {
+    const formData = serialize(
+      {
+        foo: null,
+      },
+      {
+        nullsAsUndefineds: true,
+      },
+    );
+
+    expect(formData.append).not.toHaveBeenCalled();
+    expect(formData.get('foo')).toBe(null);
+  });
+
+  test('[serialize]: boolean', () => {
+    const formData = serialize({
+      foo: true,
+      bar: false,
+    });
+
+    expect(formData.append).toHaveBeenCalledTimes(2);
+    expect(formData.append).toHaveBeenNthCalledWith(1, 'foo', true);
+    expect(formData.append).toHaveBeenNthCalledWith(2, 'bar', false);
+    expect(formData.get('foo')).toBe('true');
+    expect(formData.get('bar')).toBe('false');
+  });
+
+  test('[serialize]: boolean with booleansAsIntegers option', () => {
+    const formData = serialize(
+      {
+        foo: true,
+        bar: false,
+      },
+      {
+        booleansAsIntegers: true,
+      },
+    );
+
+    expect(formData.append).toHaveBeenCalledTimes(2);
+    expect(formData.append).toHaveBeenNthCalledWith(1, 'foo', 1);
+    expect(formData.append).toHaveBeenNthCalledWith(2, 'bar', 0);
+    expect(formData.get('foo')).toBe('1');
+    expect(formData.get('bar')).toBe('0');
+  });
+
+  test('[serialize]: integer', () => {
+    const formData = serialize({
+      foo: 1,
+    });
+
+    expect(formData.append).toHaveBeenCalledTimes(1);
+    expect(formData.append).toHaveBeenCalledWith('foo', 1);
+    expect(formData.get('foo')).toBe('1');
+  });
+
+  test('[serialize]: float', () => {
+    const formData = serialize({
+      foo: 1.01,
+    });
+
+    expect(formData.append).toHaveBeenCalledTimes(1);
+    expect(formData.append).toHaveBeenCalledWith('foo', 1.01);
+    expect(formData.get('foo')).toBe('1.01');
+  });
+
+  test('[serialize]: string', () => {
+    const formData = serialize({
+      foo: 'bar',
+    });
+
+    expect(formData.append).toHaveBeenCalledTimes(1);
+    expect(formData.append).toHaveBeenCalledWith('foo', 'bar');
+    expect(formData.get('foo')).toBe('bar');
+  });
+
+  test('[serialize]: empty string', () => {
+    const formData = serialize({
+      foo: '',
+    });
+
+    expect(formData.append).toHaveBeenCalledTimes(1);
+    expect(formData.append).toHaveBeenCalledWith('foo', '');
+    expect(formData.get('foo')).toBe('');
+  });
+
+  test('[serialize]: Object', () => {
+    const formData = serialize({
+      foo: {
+        bar: 'baz',
+        qux: 'quux',
+      },
+    });
+
+    expect(formData.append).toHaveBeenCalledTimes(2);
+    expect(formData.append).toHaveBeenNthCalledWith(1, 'foo[bar]', 'baz');
+    expect(formData.append).toHaveBeenNthCalledWith(2, 'foo[qux]', 'quux');
+    expect(formData.get('foo[bar]')).toBe('baz');
+    expect(formData.get('foo[qux]')).toBe('quux');
+  });
+
+  test('[serialize]: empty Object', () => {
+    const formData = serialize({
+      foo: {},
+    });
+
+    expect(formData.append).not.toHaveBeenCalled();
+    expect(formData.get('foo')).toBe(null);
+  });
+
+  test('[serialize]: Object in Array', () => {
+    const formData = serialize({
+      foo: [
+        {
+          bar: 'baz',
+        },
+        {
+          qux: 'quux',
+        },
+      ],
+    });
+
+    expect(formData.append).toHaveBeenCalledTimes(2);
+    expect(formData.append).toHaveBeenNthCalledWith(1, 'foo[][bar]', 'baz');
+    expect(formData.append).toHaveBeenNthCalledWith(2, 'foo[][qux]', 'quux');
+    expect(formData.get('foo[][bar]')).toBe('baz');
+    expect(formData.get('foo[][qux]')).toBe('quux');
+  });
+
+  test('[serialize]: Object in Object', () => {
+    const formData = serialize({
+      foo: {
+        bar: {
+          baz: {
+            qux: 'quux',
+          },
+        },
+      },
+    });
+
+    expect(formData.append).toHaveBeenCalledTimes(1);
+    expect(formData.append).toHaveBeenCalledWith('foo[bar][baz][qux]', 'quux');
+    expect(formData.get('foo[bar][baz][qux]')).toBe('quux');
+  });
+
+  test('[serialize]: Array', () => {
+    const formData = serialize({
+      foo: ['bar', 'baz'],
+    });
+
+    expect(formData.append).toHaveBeenCalledTimes(2);
+    expect(formData.append).toHaveBeenNthCalledWith(1, 'foo[]', 'bar');
+    expect(formData.append).toHaveBeenNthCalledWith(2, 'foo[]', 'baz');
+    expect(formData.getAll('foo[]')).toEqual(['bar', 'baz']);
+  });
+
+  test('[serialize]: empty Array', () => {
+    const formData = serialize({
+      foo: [],
+    });
+
+    expect(formData.append).not.toHaveBeenCalled();
+    expect(formData.get('foo')).toBe(null);
+  });
+
+  test('[serialize]: Array in Array', () => {
+    const formData = serialize({
+      foo: [[['bar', 'baz']]],
+    });
+
+    expect(formData.append).toHaveBeenCalledTimes(2);
+    expect(formData.append).toHaveBeenNthCalledWith(1, 'foo[][][]', 'bar');
+    expect(formData.append).toHaveBeenNthCalledWith(2, 'foo[][][]', 'baz');
+    expect(formData.getAll('foo[][][]')).toEqual(['bar', 'baz']);
+  });
+
+  test('[serialize]: Array in Object', () => {
+    const formData = serialize({
+      foo: {
+        bar: ['baz', 'qux'],
+      },
+    });
+
+    expect(formData.append).toHaveBeenCalledTimes(2);
+    expect(formData.append).toHaveBeenNthCalledWith(1, 'foo[bar][]', 'baz');
+    expect(formData.append).toHaveBeenNthCalledWith(2, 'foo[bar][]', 'qux');
+    expect(formData.getAll('foo[bar][]')).toEqual(['baz', 'qux']);
+  });
+
+  test('[serialize]: Array where key ends with "[]"', () => {
+    const formData = serialize({
+      'foo[]': ['bar', 'baz'],
+    });
+
+    expect(formData.append).toHaveBeenCalledTimes(2);
+    expect(formData.append).toHaveBeenNthCalledWith(1, 'foo[]', 'bar');
+    expect(formData.append).toHaveBeenNthCalledWith(2, 'foo[]', 'baz');
+    expect(formData.getAll('foo[]')).toEqual(['bar', 'baz']);
+  });
+
+  test('[serialize]: Array with indices option', () => {
+    const formData = serialize(
+      {
+        foo: ['bar', 'baz'],
+      },
+      {
+        indices: true,
+      },
+    );
+
+    expect(formData.append).toHaveBeenCalledTimes(2);
+    expect(formData.append).toHaveBeenNthCalledWith(1, 'foo[0]', 'bar');
+    expect(formData.append).toHaveBeenNthCalledWith(2, 'foo[1]', 'baz');
+    expect(formData.get('foo[0]')).toBe('bar');
+    expect(formData.get('foo[1]')).toBe('baz');
+  });
+
+  test('[serialize]: Array with allowEmptyArrays option', () => {
+    const formData = serialize(
+      {
+        foo: [],
+      },
+      {
+        allowEmptyArrays: true,
+      },
+    );
+
+    expect(formData.append).toHaveBeenCalledTimes(1);
+    expect(formData.append).toHaveBeenNthCalledWith(1, 'foo[]', '');
+    expect(formData.get('foo[]')).toBe('');
+  });
+
+  test('[serialize]: Date', () => {
+    const foo = new Date(2000, 0, 1, 1, 1, 1);
+    const formData = serialize({
+      foo,
+    });
+
+    expect(formData.append).toHaveBeenCalledTimes(1);
+    expect(formData.append).toHaveBeenCalledWith('foo', foo.toISOString());
+    expect(formData.get('foo')).toBe(foo.toISOString());
+  });
+
+  test('[serialize]: File', () => {
+    const foo = new File([], '');
+    const formData = serialize({
+      foo,
+    });
+
+    expect(formData.append).toHaveBeenCalledTimes(1);
+    expect(formData.append).toHaveBeenCalledWith('foo', foo);
+    expect(formData.get('foo')).toBe(foo);
+  });
+
 })
