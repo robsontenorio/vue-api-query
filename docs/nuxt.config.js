9c1d28beb8b7ae2fb16ff079407dc08c45b09bf3
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-01-16 17:50:14 -0300

    docs: fix router base

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -3,5 +3,10 @@ import theme from "@nuxt/content-theme-docs";
 export default theme({
   router: {
     base: '/vue-api-query/'
+  },
+  hooks: {
+    'vue-renderer:ssr:templateParams': function(params) {
+      params.HEAD = params.HEAD.replace('<base href="/vue-api-query/">', '')
+    }
   }
 })
