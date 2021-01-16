e762e26c7454e0e6367d57b28a296c8b9fcc5258
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-01-16 15:54:09 -0300

    feat: add typescript support

---
+++ index.d.ts
@@ -0,0 +1,639 @@
+type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+  | 'purge'
+  | 'PURGE'
+  | 'link'
+  | 'LINK'
+  | 'unlink'
+  | 'UNLINK'
+
+export interface HTTPRequestConfig {
+  method?: Method
+  url?: string
+  data?: Record<string, any> | any
+  headers?: any
+  [key: string]: any
+}
+
+export interface HTTPResponse<T> {
+  data: T
+  [key: string]: any
+}
+
+export type HTTPPromise<T = any> = Promise<HTTPResponse<T>>
+
+export interface WrappedResponse<T> {
+  data: T,
+  [key: string]: any
+}
+
+export type QueryPromise<T> = Promise<T | WrappedResponse<T>>
+
+declare class StaticModel {
+  /**
+   * Crate an instance of itself.
+   */
+  static instance<M extends typeof Model> (this: M): InstanceType<M>
+
+  /**
+   * Configuration of HTTP Instance.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#config|API Reference}
+   */
+  static config<M extends typeof Model> (this: M, config: HTTPRequestConfig): InstanceType<M>
+
+  /**
+   * Query
+   */
+
+  /**
+   * Eager load relationships.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#include|API Reference}
+   */
+  static include<M extends typeof Model> (this: M, ...relationships: string[]): InstanceType<M>
+
+  /**
+   * Eager load relationships.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#include|API Reference}
+   */
+  static include<M extends typeof Model> (this: M, relationships: any[]): InstanceType<M>
+
+  /**
+   * Eager load relationships.
+   *
+   * Alias for the [include()]{@link include} method.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#include|API Reference}
+   */
+  static with<M extends typeof Model> (this: M, ...relationships: string[]): InstanceType<M>
+
+  /**
+   * Eager load relationships.
+   *
+   * Alias for the [include()]{@link include} method.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#include|API Reference}
+   */
+  static with<M extends typeof Model> (this: M, relationships: string[]): InstanceType<M>
+
+  /**
+   * Append attributes.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#append|API Reference}
+   */
+  static append<M extends typeof Model> (this: M, ...attributes: string[]): InstanceType<M>
+
+  /**
+   * Append attributes.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#append|API Reference}
+   */
+  static append<M extends typeof Model> (this: M, attributes: string[]): InstanceType<M>
+
+  /**
+   * Set the columns to be selected.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#select|API Reference}
+   */
+  static select<M extends typeof Model> (this: M, ...columns: string[]): InstanceType<M>
+
+  /**
+   * Set the columns to be selected.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#select|API Reference}
+   */
+  static select<M extends typeof Model> (this: M, columns: string[]): InstanceType<M>
+
+  /**
+   * Set the columns to be selected.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#select|API Reference}
+   */
+  static select<M extends typeof Model> (this: M, columns: {
+    [related: string]: string[]
+  }): InstanceType<M>
+
+  /**
+   * Add a basic where clause to the query.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#where|API Reference}
+   */
+  static where<M extends typeof Model> (
+    this: M,
+    field: string | string[],
+    value: string | number | boolean
+  ): InstanceType<M>
+
+  /**
+   * Add a "where in" clause to the query.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#wherein|API Reference}
+   */
+  static whereIn<M extends typeof Model> (
+    this: M,
+    field: string | string[],
+    values: (string | number | boolean)[]
+  ): InstanceType<M>
+
+  /**
+   * Add an "order by" clause to the query.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#orderby|API Reference}
+   */
+  static orderBy<M extends typeof Model> (this: M, ...columns: string[]): InstanceType<M>
+
+  /**
+   * Add an "order by" clause to the query.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#orderby|API Reference}
+   */
+  static orderBy<M extends typeof Model> (this: M, columns: string[]): InstanceType<M>
+
+  /**
+   * Set the current page.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#page|API Reference}
+   */
+  static page<M extends typeof Model> (this: M, number: number): InstanceType<M>
+
+  /**
+   * Set the page limit.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#limit|API Reference}
+   */
+  static limit<M extends typeof Model> (this: M, number: number): InstanceType<M>
+
+  /**
+   * Build custom endpoints.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#custom|API Reference}
+   */
+  static custom<M extends typeof Model> (this: M, ...endpoint: (Model | string)[]): InstanceType<M>
+
+  /**
+   * Add custom parameters to the query.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#params|API Reference}
+   */
+  static params<M extends typeof Model> (this: M, payload: Record<string, string | number | boolean>): InstanceType<M>
+
+  /**
+   * Results
+   */
+
+  /**
+   * Execute the query and get the first result.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#first|API Reference}
+   */
+  static first<M extends typeof Model> (this: M): QueryPromise<InstanceType<M>>
+
+  /**
+   * Execute the query and get the first result.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#first-1|API Reference}
+   */
+  static $first<M extends typeof Model> (this: M): Promise<InstanceType<M>>
+
+  /**
+   * Find a model by its primary key.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#find|API Reference}
+   */
+  static find<M extends typeof Model> (this: M, id: number | string): QueryPromise<InstanceType<M>>
+
+  /**
+   * Find a model by its primary key.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#find-1|API Reference}
+   */
+  static $find<M extends typeof Model> (this: M, id: number | string): Promise<InstanceType<M>>
+
+  /**
+   * Execute the query and get all results.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#get|API Reference}
+   */
+  static get<M extends typeof Model> (this: M): QueryPromise<InstanceType<M>[]>
+
+  /**
+   * Execute the query and get all results.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#get-1|API Reference}
+   */
+  static $get<M extends typeof Model> (this: M): Promise<InstanceType<M>[]>
+
+  /**
+   * Execute the query and get all results.
+   *
+   * Alias for the [get()]{@link get} method.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#get|API Reference}
+   */
+  static all<M extends typeof Model> (this: M): QueryPromise<InstanceType<M>[]>
+
+  /**
+   * Execute the query and get all results.
+   *
+   * Alias for the [$get()]{@link $get} method.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#get-1|API Reference}
+   */
+  static $all<M extends typeof Model> (this: M): Promise<InstanceType<M>[]>
+}
+
+export class Model extends StaticModel {
+  /**
+   * Instance of the HTTP client which is used to make requests.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/model-options#http|API Reference}
+   * @see {@link https://robsontenorio.github.io/vue-api-query/installation|Installation}
+   */
+  static $http: any
+
+  constructor (...args: any[])
+
+  /**
+   * Settings
+   */
+
+  /**
+   * Instance of the HTTP client which is used to make requests.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/model-options#http|API Reference}
+   * @see {@link https://robsontenorio.github.io/vue-api-query/installation|Installation}
+   */
+  get $http (): any
+
+  /**
+   * Configuration of HTTP Instance.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#config|API Reference}
+   */
+  config (config: HTTPRequestConfig): this
+
+  /**
+   * This method can be overridden in the model to configure
+   * [object-to-formdata]{@link https://github.com/therealparmesh/object-to-formdata#usage|object-to-formdata}.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/model-options#http|API Reference}
+   * @see {@link https://github.com/therealparmesh/object-to-formdata#usage|object-to-formdata}
+   */
+  formData (): {
+    indices: boolean,
+    nullsAsUndefineds: boolean,
+    booleansAsIntegers: boolean,
+    allowEmptyArrays: boolean,
+  }
+
+  /**
+   * Resource route of the model which is used to build the query.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/model-options#resource|API Reference}
+   * @see {@link https://robsontenorio.github.io/vue-api-query/configuration#creating-the-domain-models|Configuration}
+   */
+  resource (): string
+
+  /**
+   * Primary key of the model which is used to build the query.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/model-options#primarykey|API Reference}
+   * @see {@link https://robsontenorio.github.io/vue-api-query/configuration#changing-the-primary-key|Configuration}
+   */
+  primaryKey (): string
+
+  /**
+   * Build custom endpoints.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#custom|API Reference}
+   */
+  custom (...endpoint: (Model | string)[]): this
+
+  /**
+   * This method can be used to lazy load relationships of a model and apply model instances to them.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/model-options#hasmany|API Reference}
+   * @see {@link https://robsontenorio.github.io/vue-api-query/configuration#lazy-loading-relationships|Configuration}
+   */
+  hasMany<T extends typeof Model> (model: T): InstanceType<T>
+
+  /**
+   * This method can be implemented in the model to apply model instances to eager loaded relationships.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/model-options#relations|API Reference}
+   * @see {@link https://robsontenorio.github.io/vue-api-query/configuration#eager-loaded-relationships|Configuration}
+   */
+  relations (): Record<string, typeof Model>
+
+  /**
+   * Base URL which is used and prepended to make requests.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/model-options#baseurl|API Reference}
+   * @see {@link https://robsontenorio.github.io/vue-api-query/configuration#creating-a-base-model|Configuration}
+   */
+  baseURL (): string
+
+  /**
+   * Request method which is used to make requests.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/model-options#request|API Reference}
+   * @see {@link https://robsontenorio.github.io/vue-api-query/configuration#creating-a-base-model|Configuration}
+   */
+  request (config: HTTPRequestConfig): HTTPPromise
+
+  /**
+   * From resource.
+   */
+  private _from (url: string): void
+
+  /**
+   * Helpers
+   */
+
+  /**
+   * Get the primary key of the model.
+   */
+  private getPrimaryKey (): string | number
+
+  /**
+   * Determines whether the model has an ID.
+   */
+  private hasId (): boolean
+
+  /**
+   * Determines whether the ID is valid.
+   */
+  private isValidId (id: string | number): boolean
+
+  /**
+   * The model's endpoint.
+   */
+  private endpoint (): string
+
+  /**
+   * This method can be overridden in the model to customize the name of the query parameters.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/model-options#parameternames|API Reference}
+   * @see {@link https://robsontenorio.github.io/vue-api-query/configuration#customizing-query-parameters|Configuration}
+   */
+  protected parameterNames (): {
+    include: string,
+    filter: string,
+    sort: string,
+    fields: string,
+    append: string,
+    page: string,
+    limit: string
+  }
+
+  /**
+   * Query
+   */
+
+  /**
+   * Eager load relationships.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#include|API Reference}
+   */
+  include (...relationships: string[]): this
+
+  /**
+   * Eager load relationships.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#include|API Reference}
+   */
+  include (relationships: string[]): this
+
+  /**
+   * Eager load relationships.
+   *
+   * Alias for the [include()]{@link include} method.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#include|API Reference}
+   */
+  with (...relationships: string[]): this
+
+  /**
+   * Eager load relationships.
+   *
+   * Alias for the [include()]{@link include} method.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#include|API Reference}
+   */
+  with (relationships: string[]): this
+
+  /**
+   * Append attributes.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#append|API Reference}
+   */
+  append (...attributes: string[]): this
+
+  /**
+   * Append attributes.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#append|API Reference}
+   */
+  append (attributes: string[]): this
+
+  /**
+   * Set the columns to be selected.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#select|API Reference}
+   */
+  select (...columns: string[]): this
+
+  /**
+   * Set the columns to be selected.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#select|API Reference}
+   */
+  select (columns: string[]): this
+
+  /**
+   * Set the columns to be selected.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#select|API Reference}
+   */
+  select (columns: {
+    [related: string]: string[]
+  }): this
+
+  /**
+   * Add a basic where clause to the query.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#where|API Reference}
+   */
+  where (field: string | string[], value: string | number | boolean): this
+
+  /**
+   * Add a "where in" clause to the query.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#wherein|API Reference}
+   */
+  whereIn (field: string | string[], array: (string | number | boolean)[]): this
+
+  /**
+   * Add an "order by" clause to the query.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#orderby|API Reference}
+   */
+  orderBy (...columns: string[]): this
+
+  /**
+   * Add an "order by" clause to the query.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#orderby|API Reference}
+   */
+  orderBy (columns: string[]): this
+
+  /**
+   * Set the current page.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#page|API Reference}
+   */
+  page (number: number): this
+
+  /**
+   * Set the page limit.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#limit|API Reference}
+   */
+  limit (number: number): this
+
+  /**
+   * Add custom parameters to the query.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#params|API Reference}
+   */
+  params (payload: Record<string, string | number | boolean>): this
+
+  /**
+   * Results
+   */
+
+  /**
+   * Execute the query and get the first result.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#first|API Reference}
+   */
+  first (): QueryPromise<this>
+
+  /**
+   * Execute the query and get the first result.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#first-1|API Reference}
+   */
+  $first (): Promise<this>
+
+  /**
+   * Find a model by its primary key.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#find|API Reference}
+   */
+  find (identifier: number | string): QueryPromise<this>
+
+  /**
+   * Find a model by its primary key.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#find-1|API Reference}
+   */
+  $find (identifier: number | string): Promise<this>
+
+  /**
+   * Execute the query and get all results.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#get|API Reference}
+   */
+  get (): QueryPromise<this[]>
+
+  /**
+   * Execute the query and get all results.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#get-1|API Reference}
+   */
+  $get (): Promise<this[]>
+
+  /**
+   * Execute the query and get all results.
+   *
+   * Alias for the [get()]{@link get} method.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#get|API Reference}
+   */
+  all (): QueryPromise<this[]>
+
+  /**
+   * Execute the query and get all results.
+   *
+   * Alias for the [$get()]{@link $get} method.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/query-builder-methods#get-1|API Reference}
+   */
+  $all (): Promise<this[]>
+
+  /**
+   * Common CRUD operations
+   */
+
+  /**
+   * Delete the model from the database.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/crud-operations#delete|API Reference}
+   */
+  delete (): Promise<any>
+
+  /**
+   * Save or update a model in the database, then return the instance.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/crud-operations#save|API Reference}
+   */
+  save (): Promise<this>
+
+  /**
+   * Make a `PATCH` request to update a model in the database, then return the instance.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/crud-operations#patch|API Reference}
+   */
+  patch (): Promise<this>
+
+  /**
+   * Relationship operations
+   */
+
+  /**
+   * Create a new related model.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/relationship-operations#for|API Reference}
+   */
+  for (...args: any[]): this
+
+  /**
+   * Create a new related model.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/relationship-operations#attach|API Reference}
+   */
+  attach (params: Record<string, any>): Promise<any>
+
+  /**
+   * Update a related model.
+   *
+   * @see {@link https://robsontenorio.github.io/vue-api-query/api/relationship-operations#sync|API Reference}
+   */
+  sync (params: Record<string, any>): Promise<any>
+}
