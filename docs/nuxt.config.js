727ded646b8c9a909bdedab8cde9a1060c323846
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-01-16 21:38:29 -0300

    docs: fix router base (#170)
    Temporary workaround to solve router base issue.

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -3,5 +3,16 @@ import theme from "@nuxt/content-theme-docs";
 export default theme({
   router: {
     base: '/vue-api-query/'
+  },
+  /**
+   * Workaround to fix router base issue.
+   *
+   * See https://github.com/robsontenorio/vue-api-query/issues/165
+   * See https://github.com/nuxt/content/issues/376#issuecomment-702193217
+   */
+  hooks: {
+    'vue-renderer:ssr:templateParams': function(params) {
+      params.HEAD = params.HEAD.replace('<base href="/vue-api-query/">', '')
+    }
   }
 })
