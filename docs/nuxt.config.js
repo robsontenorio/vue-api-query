48eca852e113240ff65b3b4a45e5b5d0837e530f
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-01-16 21:31:05 -0300

    docs: add workaround comment

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -4,6 +4,12 @@ export default theme({
   router: {
     base: '/vue-api-query/'
   },
+  /**
+   * Workaround to fix router base issue.
+   *
+   * See https://github.com/robsontenorio/vue-api-query/issues/165
+   * See https://github.com/nuxt/content/issues/376#issuecomment-702193217
+   */
   hooks: {
     'vue-renderer:ssr:templateParams': function(params) {
       params.HEAD = params.HEAD.replace('<base href="/vue-api-query/">', '')
