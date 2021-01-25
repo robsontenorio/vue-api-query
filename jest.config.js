939d3b39726a5d31142e827fdab8b397871b4c22
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-01-25 14:40:03 -0300

    chore(jest): use typescript

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  preset: 'ts-jest',
+  rootDir: __dirname,
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^test/(.*)$': '<rootDir>/test/$1'
+  },
+  testMatch: ['<rootDir>/test/**/*.spec.ts'],
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/build/'],
+  setupFilesAfterEnv: ['./test/setup.ts'],
   coverageDirectory: './coverage/',
-  collectCoverageFrom: ['src/*.js', '!src/index.js']
+  coverageReporters: ['json', 'lcov', 'text-summary', 'clover'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/index.ts'],
+  clearMocks: true
 }
